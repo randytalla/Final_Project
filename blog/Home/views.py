078495f9926c356424508a0743f8a809ae9c9124
randytalla <randytala2@gmail.com>
2025-03-24 07:58:32 -0700

    created the Home app

--- blog/Home/views.py
+++ blog/Home/views.py
@@ -2,11 +2,11 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 
-def home(index):
-    return HttpResponse("This is the Home Page")
+def home(request):
+    return render(request, 'home/home.html')
 
-def contact(index):
-    return HttpResponse('This is the contact page')
+def contact(request):
+    return render(request, 'home/contact.html')
 
-def about(index):
-    return HttpResponse('This is the about page')
+def about(request):
+    return render(request, 'home/about.html')
